e80e5f2b1290ad9e122646f7f55af89eb12ec680
unknown <koolmaster1408@gmail.com>
2023-02-04 23:10:43 +0700

    fix pulish version

--- src/animations.js
+++ src/animations.js
@@ -1,4 +1,4 @@
 export { default as MovieCard } from './animations/movie-card';
 export { default as PortfolioCard } from './animations/portfolio-card';
 
-export { default as UserInfoAction } from './Animations/user-info-action';
+export { default as UserInfoAction } from './animations/userinfo-action';

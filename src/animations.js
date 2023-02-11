a4c4ff1a16d758360f504ede7de12d86ae2d7dea
unknown <koolmaster1408@gmail.com>
2023-02-11 23:02:52 +0700

    Add animations material clock (NEW)

--- src/animations.js
+++ src/animations.js
@@ -4,3 +4,5 @@ export { default as PortfolioCard } from './animations/portfolio-card';
 export { default as UserInfoAction } from './animations/userinfo-action';
 
 export { default as AdvanceMiniCalendar } from './animations/advance-mini-calendar';
+
+export { default as MaterialClock } from './animations/material-clock';
